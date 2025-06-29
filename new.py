89b6b7e5f4df64f9d53fc1894dd5dcb11b560777
Lovely kumari <aec.cse.lovely@gmail.com>
2025-06-29 18:59:03 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,29 +2,23 @@ import pyautogui
 import cv2
 import numpy as np
 
-resolution = (1920, 1080)
-
+resolution = pyautogui.size() 
 codec = cv2.VideoWriter_fourcc(*"XVID")
-
 filename = "Recording.avi"
-
-fps = 60.0
-
+fps = 60.0 
 out = cv2.VideoWriter(filename, codec, fps, resolution)
-cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Live", 480, 270)
-
+cv2.namedWindow("Live", cv2.WINDOW_NORMAL) 
+cv2.resizeWindow("Live", 480, 270) 
 while True:
 
 	img = pyautogui.screenshot()
-	frame = np.array(img)
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	out.write(frame)
-	cv2.imshow('Live', frame)
+	frame = np.array(img)	
+	out.write(frame) 	
+	cv2.imshow('Live', frame) 
 	
 	if cv2.waitKey(1) == ord('q'):
 		break
 
-out.release()
+out.release() 
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
